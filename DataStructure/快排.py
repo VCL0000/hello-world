dd766ad8fb20ef2422482a83a9be555225065c3a
vcl0000 <vcl0000@163.com>
2016-03-19 20:59:21 +0800

    修改名字

---
+++ DataStructure/快排.py
@@ -0,0 +1,40 @@
+def subSort(arr, low, high):
+    key = arr[low]  # 取low处作为基准数据
+    while low < high:
+        while low < high and arr[high] >= key:
+            high -= 1
+        while low < high and arr[high] < key:  # 注1
+            arr[high] = arr[low]
+            low += 1
+            arr[low] = arr[high]
+    arr[low] = key  # 注3
+    return low
+
+
+def quickSort(arr, low, high):  # 注2
+    if low < high:
+        key_index = subSort(arr, low, high)  # 返回的是重合出的指针
+        quickSort(arr, low, key_index)  # 左边的再次排序
+        quickSort(arr, key_index + 1, high)  # 右边再次进行排序 指针有变化
+
+
+'''
+注1
+双向变换转为单项变换  指针不发生交叉变换
+活动指针仍然是high 将low指针指向的数据赋给high
+ low指针+1
+ 将higth指针指向low
+ 活动指针只有high一个
+
+ 注2
+ low为0  height为arr.len-1
+
+ 注3
+ 将基准数据移动到low指针
+'''
+
+if __name__ == "__main__":
+    array = [8, 7, 6, 5, 9, 3, 1]
+    print(array)
+    quickSort(array, 0, len(array))
+    print(array)
