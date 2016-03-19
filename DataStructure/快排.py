b7f7b1915f60f0776dd84fc29fdaf18a5aa893fe
vcl0000 <vcl0000@163.com>
2016-03-19 21:09:46 +0800

    排除代码基本语法错误

--- DataStructure/快排.py
+++ DataStructure/快排.py
@@ -1,21 +1,21 @@
-def subSort(arr, low, high):
+def sub_sort(arr, low, high):
     key = arr[low]  # 取low处作为基准数据
     while low < high:
         while low < high and arr[high] >= key:
             high -= 1
         while low < high and arr[high] < key:  # 注1
-            arr[high] = arr[low]
-            low += 1
             arr[low] = arr[high]
+            low += 1
+            arr[high] = arr[low]
     arr[low] = key  # 注3
     return low
 
 
-def quickSort(arr, low, high):  # 注2
+def quick_sort(arr, low, high):  # 注2
     if low < high:
-        key_index = subSort(arr, low, high)  # 返回的是重合出的指针
-        quickSort(arr, low, key_index)  # 左边的再次排序
-        quickSort(arr, key_index + 1, high)  # 右边再次进行排序 指针有变化
+        key_index = sub_sort(arr, low, high)  # 返回的是重合出的指针
+        quick_sort(arr, low, key_index)  # 左边的再次排序
+        quick_sort(arr, key_index + 1, high)  # 右边再次进行排序 指针有变化
 
 
 '''
@@ -36,5 +36,5 @@ def quickSort(arr, low, high):  # 注2
 if __name__ == "__main__":
     array = [8, 7, 6, 5, 9, 3, 1]
     print(array)
-    quickSort(array, 0, len(array))
+    quick_sort(array, 0, len(array)-1)
     print(array)
