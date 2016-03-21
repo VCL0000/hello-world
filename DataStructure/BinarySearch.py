8e6c97b5da2f3259cc41b92bbcbdb76b1a95e407
vcl0000 <vcl0000@163.com>
2016-03-21 17:31:18 +0800

    简单注释补充

--- DataStructure/BinarySearch.py
+++ DataStructure/BinarySearch.py
@@ -2,13 +2,11 @@ def BinarySearch(arr, m):
     low = 0
     high = len(arr) - 1
     while (low < high):
-        mid = (low + high) // 2
+        mid = (low + high) // 2  # //为整除
         if arr[mid] > m:
             high = mid - 1
         elif arr[mid] < m:
             low = mid + 1
         else:
             return mid
-    return -1
-
-# arr = [1, 2, 3, 5, 7, 8, 9]
+    return -1  # 不存在返回的
